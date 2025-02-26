49352d3df0c051d01bacc75d081366ee1406bfee
stih01 <inoventica@bk.ru>
2025-02-26 20:02:28 +0700

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_PH1M_EA_9970A.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_PH1M_EA_9970A-user \
+    twrp_PH1M_EA_9970A-userdebug \
+    twrp_PH1M_EA_9970A-eng
