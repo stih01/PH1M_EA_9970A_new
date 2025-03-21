bad30bb2f94ee14118a3b7a43fc55dc29ea0fa78
stih01 <inoventica@bk.ru>
2025-03-21 19:11:16 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,8 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tpv/PH1M_EA_9970A
+LOCAL_PATH := device/Philips/PH1M_EA_9970A
+
 # A/B
+AB_OTA_UPDATER := true
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -16,13 +19,20 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl.recovery
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt5895\
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl.mt5895
+#    libgptutils \
+#    libz \
+#    libcutils
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl \
+    android.hardware.fastboot@1.0-impl.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -30,3 +40,22 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Usb HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+# Additional binaries & libraries needed for recovery
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so
