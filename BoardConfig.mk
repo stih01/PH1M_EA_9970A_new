fd91f9a67e693b6057b3bedb13b427b82026e170
stih01 <inoventica@bk.ru>
2025-03-21 20:27:52 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/Philips/PH1M_EA_9970A
+DEVICE_PATH := device/Philips/PH1M_EA_9970A_new
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
